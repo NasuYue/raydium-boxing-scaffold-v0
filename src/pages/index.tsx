3bd2e46bedac42fbb05d4b698d2bade4f1e84c91
Dan Hsu <hsu86110@gmail.com>
2022-04-23 14:55:44 +0800

    feature: component setting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Head from "next/head";
+import { HomeView } from "../views";
+import Calculator from "components/Calculator";
 
 const Home: NextPage = (props) => {
   return (
@@ -8,6 +10,8 @@ const Home: NextPage = (props) => {
         <title>Box: A Raydium Solution</title>
         <meta name="description" content="Box: A Raydium Solution" />
       </Head>
+      <HomeView />
+      <Calculator/>
     </div>
   )
 }
