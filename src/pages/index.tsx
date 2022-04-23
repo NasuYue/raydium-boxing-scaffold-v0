1d04c9a905a1fa623aa8ef68c936d3c8b661dfce
Ting <tchangysl@gmail.com>
2022-04-23 14:53:28 +0800

    Remove unused files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,15 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import { HomeView } from "../views";
+import type { NextPage } from 'next'
+import Head from 'next/head'
 
 const Home: NextPage = (props) => {
   return (
     <div>
       <Head>
-        <title>Solana Scaffold</title>
-        <meta
-          name="description"
-          content="Solana Scaffold"
-        />
+        <title>Box: A Raydium Solution</title>
+        <meta name="description" content="Box: A Raydium Solution" />
       </Head>
-      <HomeView />
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
