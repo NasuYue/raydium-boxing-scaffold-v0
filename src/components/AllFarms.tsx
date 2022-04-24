ddaf6ef6feab78cfab3683462acef41267126732
Ting <tchangysl@gmail.com>
2022-04-24 13:19:24 +0800

    Add in bRAY-RAY Farm data

--- src/components/AllFarms.tsx
+++ src/components/AllFarms.tsx
@@ -1,8 +1,22 @@
 import { FC } from 'react'
 import useSwr from 'swr'
 import Farm from './Farm'
+import boxLogo from '../assets/symbal/box.png'
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
+
+const RAY_MINT = '4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R'
+
+const BRAY_RAY_DATA = {
+  pairing: 'bRAY-RAY',
+  lpToken: {
+    base: { src: boxLogo },
+    quote: { mint: RAY_MINT },
+  },
+  lpPool: { liquidity: 123456787 },
+  rewardMints: ['a'],
+  realApr: '88.88',
+}
 
 const AllFarms: FC = () => {
   const { data, error } = useSwr('/api/calculate', fetcher)
@@ -17,6 +31,7 @@ const AllFarms: FC = () => {
 
   return (
     <div className="flex flex-wrap items-center justify-center m-6">
+      <Farm pairing={BRAY_RAY_DATA.pairing} farm={BRAY_RAY_DATA} />
       {Object.entries(data).map(([key, value]: [string, any]) => (
         <Farm key={key} pairing={key} farm={value} />
       ))}
